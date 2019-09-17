8ecfbe9ac1d96f3257336955ebc3d7c6f84d7461
fstennet <franklinstennett@gmail.com>
2019-09-16 19:46:23 -0600

    changed dataset

--- classificator.py
+++ classificator.py
@@ -5,5 +5,33 @@ import tensorflow as tf
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+  dataframe = dataframe.copy()
+  labels = dataframe.pop('position')
+  dataframe.pop('name')
+  ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+  #print(labels)
+  if shuffle:
+    ds = ds.shuffle(buffer_size=len(dataframe))
+  ds = ds.batch(batch_size)
+  return ds
+
 dataframe = pd.read_csv('playerDataSet.csv', engine='python', encoding='ISO-8859-1', error_bad_lines=False)
-print(dataframe.head())
+#print(dataframe.head())
+
+train, test = train_test_split(dataframe, test_size=0.2)
+train, val = train_test_split(train, test_size=0.2)
+
+print(len(train), 'train examples')
+print(len(val), 'validation examples')
+print(len(test), 'test examples')
+
+batch_size = 1 # A small batch sized is used for demonstration purposes
+train_ds = df_to_dataset(train, batch_size=batch_size)
+val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+for feature_batch, label_batch in train_ds.take(1):
+  print('Every feature:', list(feature_batch.keys()))
+  print('A batch of dribbling:', feature_batch['dribbling'])
+  print('A batch of targets:', label_batch )
--- data_curator.py
+++ data_curator.py
@@ -5,13 +5,19 @@ arg = input('ENTER for minimal: ')
 
 start = time.time()
 new_list = []
-with open('playerDB.csv', newline='') as database:
+with open('playerDB.csv', newline='', encoding='ISO-8859-1') as database:
     print('Reading from playerDB.csv')
     file_iterator = csv.reader(database)
     for row in file_iterator:
         if not arg:
             if row[7] == 'GK' or row[7] == 'CAM' or row[0] == 'id':
-                new_row = [row[62], row[10], row[11], row[23], row[7]]
+                if row[7] == 'GK':
+                    position = 0
+                elif row[7] == 'CAM':
+                    position = 1
+                else:
+                    position = row[7]
+                new_row = [row[62], row[10], row[11], row[23], position]
                 new_list.append(new_row)
         else:
             new_row = [row[62], row[10], row[11], row[23], row[7]]
